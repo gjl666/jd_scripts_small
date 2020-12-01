0a56dc8cbf8cb57a0e17de0c16ff29d9c8a1570c
gjl666 <71857306+gjl666@users.noreply.github.com>
2020-12-01 20:06:57 +0800

    Update jdCookie.js

--- jdCookie.js
+++ jdCookie.js
@@ -4,11 +4,11 @@
 //此处填写京东账号cookie。
 //注：github action用户cookie填写到Settings-Secrets里面，新增JD_COOKIE，多个账号的cookie使用`&`隔开或者换行
 let CookieJDs = [
-  '',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
+  'pt_key=AAJfxh5VADBJVrADjiIlTdg4aPQL3J8xVoZMNerasIZ8pEyTJFbqGuwtRDiF9UH_DiQngUH86jE; pt_pin=jd_481bdca8990ee;',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
   '',//账号二ck,例:pt_key=XXX;pt_pin=XXX;如有更多,依次类推
   '',
   '',
-  'pt_key=AAJfxh5VADBJVrADjiIlTdg4aPQL3J8xVoZMNerasIZ8pEyTJFbqGuwtRDiF9UH_DiQngUH86jE; pt_pin=jd_481bdca8990ee;',
+  '',
 ]
 // 判断github action里面是否有京东ck
 if (process.env.JD_COOKIE) {
