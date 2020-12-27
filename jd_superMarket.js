e9fde28ef4d8468998880ef252bc4668a6ce032b
a370263995 <63954316+a370263995@users.noreply.github.com>
2020-12-27 12:02:06 +0800

    Update jd_superMarket.js

--- jd_superMarket.js
+++ jd_superMarket.js
@@ -321,27 +321,27 @@ async function businessCircleActivity() {
         let Teams = [
           {
             "teamId": "-4msulYas0O2JsRhE-2TA5XZmBQ_1604247312176",
-            "inviteCode": "-4msulYas0O2JsRhE-2TA5XZmBQ"
+            "inviteCode": "eU9Ya-i1NPwv92zUmSUR3g"
           },
           {
             "teamId": "Ih4-a-mwZPUj9Gy6iw_1604277683224",
-            "inviteCode": "eU9Yar_mb_9z92_WmXNG0w"
+            "inviteCode": "eU9Ya-i1NPwv92zUmSUR3g"
           },
           {
             "teamId": "eU9Ya77gZK5z-TqHn3UWhQ_1604277779750",
-            "inviteCode": "eU9YaOnjYK4j-GvWmXIWhA"
+            "inviteCode": "eU9Ya-i1NPwv92zUmSUR3g"
           },
           {
             "teamId": "eU9Ya-y2N_5z9DvXwyIV0A",
-            "inviteCode": "eU9YaLm0bq4i-TrUzSUUhA"
+            "inviteCode": "eU9Ya-i1NPwv92zUmSUR3g"
           },
           {
             "teamId": "eU9Ya-y2N_5z9DvXwyIV0A",
-            "inviteCode": "aURoM7PtY_Q"
+            "inviteCode": "eU9Ya-i1NPwv92zUmSUR3g"
           },
           {
             "teamId": "eU9Ya-y2N_5z9DvXwyIV0A",
-            "inviteCode": "eU9YaeS3Z6ol8zrRmnMb1Q"
+            "inviteCode": "eU9Ya-i1NPwv92zUmSUR3g"
           }
         ]
         Teams = $.updatePkActivityIdRes['Teams'] || Teams;
