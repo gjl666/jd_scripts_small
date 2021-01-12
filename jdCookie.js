82d875d157dac7044f6e14b0a70c53beb3dc258b
a370263995 <63954316+a370263995@users.noreply.github.com>
2021-01-12 09:01:41 +0800

    Update jdCookie.js

--- jdCookie.js
+++ jdCookie.js
@@ -4,7 +4,7 @@
 //此处填写京东账号cookie。
 //注：github action用户cookie填写到Settings-Secrets里面，新增JD_COOKIE，多个账号的cookie使用`&`隔开或者换行
 let CookieJDs = [
-  'pt_key=AAJf1K9YADCOZ2egbGs2vOKWwN0TL0XmnTZJ95nXCZnVT3IJZW42ZMrNQGy1tXTmcEgXvZgixAE;pt_pin=jd_747b09621bf28;',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
+  'pt_key=AAJf_PQuADAsNIW8A5DIRsCwsQJeX6dgs9Oo8ZU_ceacxGPAVqnFjFhEoADS3RpUj12Wgdp88co; pt_pin=jd_747b09621bf28;',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
   'pt_key=AAJf5tnKADBsH9JuNdnRdCwF9sozKoYt7CGTAP2ZE4IYTHcbnfTfsC9z7mZl6tR0qgMbVrptZNc; pt_pin=jd_6127dbf3213c4;',
   'pt_key=AAJf5_-pADBFEkVzkbwYvPIkBkUv5Iab8m3D_iUvPWMLeKIDHWB2aC7aNsx4hkTsM4u1YFCbAMg; pt_pin=jd_7fc1e0fb0afd0;',
   'pt_key=AAJf6B9eADAZOAhBqFVB5KE-HwFiJIsH_O6raG-1aGEHZe287EE1JuLAPQZHplVGIl-FdULx9NU; pt_pin=jd_XcSCPXDlmrYz;',
